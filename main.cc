27a1f90a3d34821a438b36b704bce7af8a14b3f9
nohelia <ntacca@unsa.edu.pe>
2022-10-27 06:23:22 -0500

    intento del main

--- main.cc
+++ main.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include "arrayStack.h"
+using namespace std;
 
 int main(){
-  ArrayStack *pstack = new ArrayStack();
+  ArrayStack pstack = new ArrayStack();
   T first = 5; 
   pstack->push(first);
   cout<<pstack->data<<" ";
