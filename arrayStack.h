215cd6c2c3d0a4e82a50bb0f90e2ccf3dd63b35f
nohelia <ntacca@unsa.edu.pe>
2022-11-03 01:07:42 -0500

    corrección de código

--- arrayStack.h
+++ arrayStack.h
@@ -5,13 +5,14 @@
 template<typename T>
 class ArrayStack: public Stack<T>{
   private:
-    T data [];
+    T* data;
     int size; //memory size
     T* sp; //stackPointer		
     //no hay necesidad de poner virtual porque ya heredamos la virtualidad.
     void resize();
   public:
-    ArrayStack(int s):size{s}, data{new T[size]}, sp{data}{}
+    ArrayStack(int);
+    //ArrayStack(int s):size{s}, data{new T[size]}, sp{data}{}
     void push(T);
     void pop();
     T top();
