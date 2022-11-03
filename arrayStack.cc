612fd6128f2585ca101b90faaec0b1537afae501
nohelia <ntacca@unsa.edu.pe>
2022-11-03 01:07:20 -0500

    corrección de código

--- arrayStack.cc
+++ arrayStack.cc
@@ -1,21 +1,35 @@
 #include "arrayStack.h"
-#include <assert.h>"
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+template<typename T>
+//constructor
+ArrayStack<T>:: ArrayStack(int s){
+  size = s;
+  data = new T[s];
+  sp = data;
+  assert(empty());
+  pop();
+}
+
 template<typename T>
 void ArrayStack<T>::push(T e){
   int numelem = sp - data; //cantidad de elementos , resta con aritmetica de punteros
-  assert (numelem == size);
-  resize();//debo reservar más memoria, data apuntará a un arreglo nuevo más grande|:
+  if (numelem == size)
+    resize();//debo reservar más memoria, data apuntará a un arreglo nuevo más grande|:
   *sp = e; //almacena T en la posición libre sp
   sp++;
-  assert(numelem + 1 == size); 
+  assert(*(sp-1) == e);
+  assert(!empty());
 }
 
 template<typename T>
 void ArrayStack<T>::pop(){
-  assert (!empty());
-  //  return;
+  if(empty()){
+    return;
+  }
   sp--;
-  assert(numelem - 1 == size);
 }
 
 template <typename T>
@@ -27,13 +41,12 @@ bool ArrayStack<T>::empty(){
 
 template<typename T>
 void ArrayStack<T>::resize(){
-  T newData[] = new T[size + FACTOR];
-  assert(FACTOR != 0); //Precondition
+  T *newData = new T[size + FACTOR];
   for(int i=0; i < size; i++)
     newData[i] = data[i];
   delete[] data;
   data = newData;
-  sp = data + size;
+  //sp = data + size;
   size += FACTOR;
 }
 
@@ -41,6 +54,27 @@ template<typename T>
 T ArrayStack<T>::top(){
   assert(!empty());
   return *(sp - 1);
+}
+
+int main(){
+  ArrayStack<int> arr(8);
+  arr.push(1);
+  arr.push(2);
+  arr.push(3);
+  arr.push(4);
+  arr.push(5);
+  arr.push(6);
+  arr.push(7);
+
+  cout<<arr.top()<<endl;
+  arr.pop();
+  cout<<arr.top()<<endl;
+  arr.pop();
+  cout<<arr.top()<<endl;
+  arr.pop();
+  cout<<arr.top()<<endl;
+  arr.pop();
+  cout<<arr.top()<<endl;
 }
 
 //implementar precondiciones y postcondiciones
